c5df3534aa66daf3f253e6f79ae5b2e40f4ea459
castilhoserafim <wesleycserafim@gmail.com>
2025-06-18 18:53:19 -0300

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,50 +1,56 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
-import styles from './Post.module.css'
 
-export function Post({ author, content }) {
+import styles from './Post.module.css';
+
+export function Post({ author, content, publishedAt }) {
   return (
-     <article className={styles.post}>
+    <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar hasBorder src="https://github.com/castilhoserafim.png" alt="" />
+          <Avatar hasBorder src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-              <strong>{author}</strong> {/* Aqui usa a prop */}
-              <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="25 de Abril às 18:48" dateTime="2025-04-25 18:48:22">Publicado há 1h</time>
+        <time 
+          title={publishedAt.toLocaleString()}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedAt.toLocaleDateString()}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>{content}</p> {/* Aqui usa a prop */}
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-            <a href="">#novoprojeto</a>{' '}
-            <a href="">#nlw</a>{' '}
-            <a href="">#rocketseat</a>{' '}
-         </p>
+        {content.map((line, index) => {
+          if (line.type === 'paragraph') {
+            return <p key={index}>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return (
+              <p key={index}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
-          <strong>Deixe seu feedback</strong>
+        <strong>Deixe seu feedback</strong>
 
-          <textarea 
-            placeholder='Deixe um comentário'
-          />
+        <textarea placeholder="Deixe um comentário" />
 
-          <footer>
-            <button type="submit" className={styles.hidden}>Publicar</button>
-          </footer>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
       </form>
 
       <div className={styles.commentList}>
-         <Comment />
-         <Comment />
-         <Comment />
+        <Comment />
+        <Comment />
       </div>
-     </article>
-  )
+    </article>
+  );
 }
